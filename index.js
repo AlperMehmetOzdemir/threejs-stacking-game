826ff2c5cbbf69689d789657c515ae4f82c5bb58
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-07-27 21:11:14 +0300

    Game setup

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
 // const THREE = ("three");
 // const CANNON = require("cannon");
 
-// Scene
-const scene = new THREE.Scene();
+// ThreeJS globals
+let camera, scene, renderer;
+const originalBoxSize = 3;
 
-// Add a cube to the scene
-const geometry = new THREE.BoxGeometry(3,1,3)
-const material = new THREE.MeshLambertMaterial({color: 0xfb8e00});
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.set(0,0,0);
-scene.add(mesh);
+function init() {
+  scene = new THREE.Scene();
 
-// setup lights
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-scene.add(ambientLight);
+  // Foundation
+  addLayer(0, 0, originalBoxSize, originalBoxSize);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
-directionalLight.position.set(10,20,0);
-scene.add(directionalLight);
+  // First layer
+  addLayer(-10, 0, originalBoxSize, originalBoxSize, "x");
 
-// Camera
-const aspectRatio = window.innerHeight / window.innerWidth;
-const width = 10;
-const height =  width * aspectRatio;
-const camera = new THREE.OrthographicCamera(
-  width / -2,
-  width / 2,
-  height / 2,
-  height / -2,
-  1,
-  100
-);
+  // Set up lights
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+  scene.add(ambientLight);
 
-camera.position.set(4,4,4);
-camera.lookAt(0,0,0);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+  directionalLight.position.set(10, 20, 0);
+  scene.add(directionalLight);
 
-// Renderer
-const renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
+  // Camera
+  const aspectRatio = window.innerHeight / window.innerWidth;
+  const width = 10;
+  const height = width * aspectRatio;
+  const camera = new THREE.OrthographicCamera(
+    width / -2,
+    width / 2,
+    height / 2,
+    height / -2,
+    1,
+    100
+  );
 
-// Add it to HTML
-document.body.appendChild(renderer.domElement);
+  camera.position.set(4, 4, 4);
+  camera.lookAt(0, 0, 0);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+
+  // Add it to HTML
+  document.body.appendChild(renderer.domElement);
+}
